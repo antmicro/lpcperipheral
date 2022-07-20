02ab363b1970ecb69d40184434d0f930c7c00720
Tomasz Michalak <tmichalak@antmicro.com>
2022-07-20 13:44:33 +0200

    Update installation dependencies
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
-import setuptools
+from setuptools import setup
 
-if __name__ == "__main__":
-    setuptools.setup()
+setup(
+    name='lpcperipheral',
+    version='1.0',
+    author='IBM Corp.',
+    description='LPC peripheral that implements LPC IO and FW cycles',
+    url='https://github.com/OpenPOWERFoundation/lpcperipheral',
+    license='Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)',
+    install_requires=[
+        'amaranth',
+        'amaranth-soc@git+https://github.com/amaranth-lang/amaranth-soc.git'
+    ],
+)
